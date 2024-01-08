27e439fd17001a7132b09a032f0c88daa3d97526
gouzil <66515297+gouzil@users.noreply.github.com>
2024-01-08 14:18:07 +0800

    🐛 fix: check if src_dir is a subdirectory of dst_dir (#10)
    Co-authored-by: Nyakku Shigure <sigure.qaq@gmail.com>

--- watchfs/__main__.py
+++ watchfs/__main__.py
@@ -75,6 +75,13 @@ def parse_sync_mapping(sync_mapping: str) -> Result[tuple[str, str], ParseError]
     if len(splited_sync_mapping) != 2:
         return Err(ParseError("Invalid sync mapping."))
     src_dir, dst_dir = sync_mapping.split(":")
+
+    # check if src_dir is a subdirectory of dst_dir
+    abs_src_dir = Path(src_dir).resolve()
+    abs_dst_dir = Path(dst_dir).resolve()
+    if abs_src_dir in abs_dst_dir.parents:
+        return Err(ParseError(f"src_dir({abs_src_dir}) is a subdirectory of dst_dir({abs_dst_dir})."))
+
     return Ok((src_dir, dst_dir))
 
 
